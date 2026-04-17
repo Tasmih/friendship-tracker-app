2f744383f5c6f778e928108bcca618a06a1bb331
Tasmih <tasmih.mow20@gmail.com>
2026-04-17 13:25:49 +0600

    integrate toast notifications for user interactions

--- src/Components/MyNavLink.jsx
+++ src/Components/MyNavLink.jsx
@@ -11,7 +11,7 @@ const MyNavLink = ({ to, children, onClick }) => {
         ${
           isActive
             ? "bg-emerald-900 text-white"
-            : "text-gray-500 hover:bg-green-300 hover:text-gray-900"
+            : "text-gray-50 hover:bg-green-300 hover:text-gray-900"
         }`
       }
     >
